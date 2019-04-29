d20fb558747b355d977bf2a3ff4c2cd45d479b27
Akash Potdar <akashspotdar7@gmail.com>
2019-04-29 16:24:06 +0530

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <Page>
+        <GridLayout>
+            <FAQs :msg="msg" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from "vue-property-decorator";
+import FAQs from './FAQs.vue';
+
+  @Component({
+    components: {
+        FAQs
+    }  
+  })
+  export default class App extends Vue {
+    data() {
+      return {
+        msg: 'Welcome to Airim Native-app!'
+      }
+    }
+  }
+</script>
---
+++ app/components/FAQs.vue
@@ -0,0 +1,20 @@
+<template>
+  <Label class="message" :text="msg"/>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+    @Component({})
+    export default class FAQs extends Vue {
+        @Prop() private msg: string;
+    }
+</script>
+<style lang="scss" scoped>
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
