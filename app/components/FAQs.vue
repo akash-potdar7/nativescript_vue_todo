21060f08d2d86d141c006c18c13a42fc53686592
Akash Potdar <akashspotdar7@gmail.com>
2019-04-29 23:01:33 +0530

    Installed axios and mocked a url for a list rendering

--- app/components/FAQs.vue
+++ app/components/FAQs.vue
@@ -4,11 +4,15 @@
             <Label class="message" textWrap="true" :text="msg"/>
             <Button text="Return" @tap="goBack"></Button>
             <GridLayout>
+                <FlexboxLayout flexDirection="column" justifyContent="space-around">
+                    <ActivityIndicator :busy="isBusy" alignSelf="center" width="40" height="40" />
+                </FlexboxLayout>
                 <ListView for="p in pokemon" class="list-group">
                     <v-template>
-                    <StackLayout class="list-group-item">
-                        <Label :text="p.name" />
-                    </StackLayout>
+                        <StackLayout class="list-group-item">
+                            <Label :text="p.name" />
+                            <Label :text="p.url" />
+                        </StackLayout>
                     </v-template>
                 </ListView>
             </GridLayout>
@@ -19,12 +23,14 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import App from './App.vue';
+import axios from "axios";
 
     @Component({})
     export default class FAQs extends Vue {
         // @Prop()
             private msg: string;
         private pokemon: any[] = [];
+        private isBusy: boolean = false;
 
         constructor() {
             super();
@@ -32,12 +38,36 @@ import App from './App.vue';
         }
 
         beforeMount() {
-            console.log('beforeUpdate')
+            console.log('in beforeMount');
+            this.initFetch();
+            this.fetchTopFAQs();
         }
 
         goBack() {
             this.$navigateTo(App, {clearHistory: true});
         }
+
+        toggleSpinner() {
+            this.isBusy = !this.isBusy;
+        }
+
+        private async initFetch() {
+            this.toggleSpinner();
+            await this.fetchTopFAQs();
+            this.toggleSpinner();
+        }
+
+        private async fetchTopFAQs(): Promise<any> {
+            return axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+                .then(response => {
+                    console.log('pokemons', response.data);
+                    this.pokemon = response.data.results;
+                })
+                .catch(error => {
+                    console.log('Network call failed', error);
+                });
+        }
+
     }
 </script>
 <style lang="scss" scoped>
---
+++ app/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+    urls: {
+        topFaqs: '/widget/topfaq',
+    },
+};
