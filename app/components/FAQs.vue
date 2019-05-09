ebcf60166d3f1e53afc312f787d82d48d2ee4f76
Akash Potdar <akashspotdar7@gmail.com>
2019-05-09 16:38:31 +0530

    FAQs List view

--- app/components/FAQs.vue
+++ app/components/FAQs.vue
@@ -1,23 +1,24 @@
 <template>
-   <Page>
-        <StackLayout>
-            <Label class="message" textWrap="true" :text="msg"/>
-            <Button text="Return" @tap="goBack"></Button>
-            <GridLayout>
-                <FlexboxLayout flexDirection="column" justifyContent="space-around">
+    <Page class="page" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+		<GridLayout orientation="vertical" width="100%" height="100%" columns="*" rows="*,auto">
+			<StackLayout col="0" row="0" backgroundColor="#f8f8f8">
+                <Label class="font-weight-bold" marginTop="10" paddingLeft="10" fontSize="20" color="#333333" :text="headline"></Label>
+                <FlexboxLayout v-if="isBusy" flexDirection="column" justifyContent="space-around">
                     <ActivityIndicator :busy="isBusy" alignSelf="center" width="40" height="40" />
                 </FlexboxLayout>
-                <ListView for="p in pokemon" class="list-group">
+                <ListView class="list-group" v-for="(faq, index) in faqs" :key="index" marginTop="10" backgroundColor="#f8f8f8">
                     <v-template>
-                        <StackLayout class="list-group-item">
-                            <Label :text="p.name" />
-                            <Label :text="p.url" />
-                        </StackLayout>
+                        <GridLayout orientation="horizontal" class="list-group-item" rows="auto" columns="auto,*,auto">
+                            <StackLayout col="1" row="0" height="100" marginLeft="16" padding="8">
+                                <Label marginTop="4" paddingRight="4" :text="faq.question" class="list-group-item-heading font-weight-bold" color="#333333"></Label>
+                                <HtmlView textWrap="true" class="p-10" marginTop="4" :html="faq.answer" color="#999999"></HtmlView>
+                            </StackLayout>
+                        </GridLayout>
                     </v-template>
                 </ListView>
-            </GridLayout>
-        </StackLayout>
-    </Page>
+            </StackLayout>
+		</GridLayout>
+	</Page>
 </template>
 
 <script lang="ts">
@@ -27,14 +28,14 @@ import axios from "axios";
 
     @Component({})
     export default class FAQs extends Vue {
-        // @Prop()
-            private msg: string;
-        private pokemon: any[] = [];
+        
+        private faqs: any[] = [];
         private isBusy: boolean = false;
+        private headline: string;
 
         constructor() {
             super();
-            this.msg = "Yet to build FAQs list";
+            this.headline = "Frequently asked questions";
         }
 
         beforeMount() {
@@ -58,10 +59,114 @@ import axios from "axios";
         }
 
         private async fetchTopFAQs(): Promise<any> {
-            return axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+            return axios.get('../assets/faqs.json')
                 .then(response => {
-                    console.log('pokemons', response.data);
-                    this.pokemon = response.data.results;
+                    const faqs = {
+                                "top_faqs": [
+                                {
+                                    "id": 117,
+                                    "question": "Who are Airim's founders?",
+                                    "answer": "<p>Airim team comprises of Sales Strategy and Data Science professionals with prior experience in fast-growing startups as well as Fortune 100 companies.</p>",
+                                    "tags": [
+                                    "cxo",
+                                    "cto",
+                                    "ceo",
+                                    "directors"
+                                    ],
+                                    "account_id": 1059401424129564,
+                                    "category_id": 1517,
+                                    "rank": null,
+                                    "actions": [
+                                    {
+                                        "link_type": "web_url",
+                                        "web_url": "https://www.linkedin.com/company/13299648/admin/",
+                                        "action_id": 1151,
+                                        "widget_id": null,
+                                        "label": "View Team Page"
+                                    }
+                                    ]
+                                },
+                                {
+                                    "id": 2998,
+                                    "question": "Can I integrate multiple knowledge bases within the same subscription?",
+                                    "answer": "<p><span style=\"color: rgb(74, 74, 74);\">Yes, it is possible. Simply click Import in your Content Manager and add your knowledge bases one-by-one. You can also add more FAQs one-by-one manually after importing your entire knowledge base content.</span></p>",
+                                    "tags": [],
+                                    "account_id": 1059401424129564,
+                                    "category_id": 2217,
+                                    "rank": null,
+                                    "actions": [
+                                    {
+                                        "link_type": "web_url",
+                                        "web_url": "https://app.airim.co/home/faqsManager/",
+                                        "action_id": 1308,
+                                        "widget_id": null,
+                                        "label": "Import Now"
+                                    }
+                                    ]
+                                },
+                                {
+                                    "id": 2874,
+                                    "question": "How do I create multiple accounts?",
+                                    "answer": "<p>If you run an agency, or handle multiple websites, you can sign up with different emails for each account. Once you have done that, just write to hi@airim.co and we will link all your accounts, so that you can login with one email address for all accounts.</p>",
+                                    "tags": [],
+                                    "account_id": 1059401424129564,
+                                    "category_id": 2217,
+                                    "rank": null,
+                                    "actions": [
+                                    {
+                                        "link_type": "web_url",
+                                        "web_url": "https://app.airim.co/signup",
+                                        "action_id": 1152,
+                                        "widget_id": null,
+                                        "label": "Start Now for Free"
+                                    }
+                                    ]
+                                },
+                                {
+                                    "id": 99,
+                                    "question": "What is Airim used for?",
+                                    "answer": "<p>Airim saves time for your users by surfacing the right FAQ for them at the right time.</p><p>Website visitors love interacting with Airim as it helps them explore websites with ease. And CXOs love it because it saves time for their customers.</p><p>Airim's analytics is immensely powerful, and product marketers use it to know what their customers are looking for, what excites them and what makes them lose interest.</p>",
+                                    "tags": [
+                                    "What is Airim",
+                                    "what is the use"
+                                    ],
+                                    "account_id": 1059401424129564,
+                                    "category_id": 1517,
+                                    "rank": null,
+                                    "actions": [
+                                    {
+                                        "link_type": "web_url",
+                                        "web_url": "https://app.airim.co/signup",
+                                        "action_id": 300,
+                                        "widget_id": 0,
+                                        "label": "Start Now for Free"
+                                    }
+                                    ]
+                                },
+                                {
+                                    "id": 115,
+                                    "question": "How long will it take to set up?",
+                                    "answer": "<p>Less than a minute actually. Just sign up, provide your knowledge base URL and embed your unique Airim in your website or product.</p>",
+                                    "tags": [
+                                    "time"
+                                    ],
+                                    "account_id": 1059401424129564,
+                                    "category_id": 1517,
+                                    "rank": null,
+                                    "actions": [
+                                    {
+                                        "link_type": "web_url",
+                                        "web_url": "https://app.airim.co/signup",
+                                        "action_id": 301,
+                                        "widget_id": 0,
+                                        "label": "Start Now for Free"
+                                    }
+                                    ]
+                                }
+                                ]
+                            };
+                            this.faqs = faqs.top_faqs;
+                    console.log('faqs', response.data);
                 })
                 .catch(error => {
                     console.log('Network call failed', error);
